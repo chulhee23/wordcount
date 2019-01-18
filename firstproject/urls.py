9349a6073d52f99ddf2eb1f48ad11d9a50481700
MacBook13 <macbook13@MacBook13ui-MacBook-Pro.local>
2019-01-18 23:03:11 +0900

    wordcount 마무리

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 import wordcount.views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',wordcount.views.home, name="home"),
-    path('about/',wordcount.views.about, name="about"),
-    
+    path('', wordcount.views.home, name="home"),
+    path('about/', wordcount.views.about, name="about"),
+    path('result/', wordcount.views.result, name="result"),
 
 ]
--- wordcount/views.py
+++ wordcount/views.py
@@ -9,4 +9,19 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
-    
+     
+def result(request):
+    text = request.GET['fulltext']
+    words = text.split()
+    # 총 단어수 = list의 길이
+
+    word_dic = {}
+
+    for word in words:
+        if word in word_dic:
+            word_dic[word]+=1
+        else:
+            # add 2 dic
+            word_dic[word]=1
+
+    return render(request, 'result.html', { 'full':text, 'total':len(words), 'dic' : word_dic.items() } )
