f87901be3c652f8b303b6caa4b676b91b14e4694
MacBook13 <macbook13@MacBook13ui-MacBook-Pro.local>
2019-01-18 22:31:22 +0900

    link update

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -5,5 +5,7 @@ import wordcount.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',wordcount.views.home, name="home"),
+    path('about/',wordcount.views.about, name="about"),
+    
 
 ]
--- wordcount/views.py
+++ wordcount/views.py
@@ -6,4 +6,7 @@ def home(request):
     return render(request, 'home.html')
     # render 함수는 3개의 인자를 받아줄 수 있다. 
     # 3번째 인자는 dic형 자료를 받아올 수 있다.
+
+def about(request):
+    return render(request, 'about.html')
     
