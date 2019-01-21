fb69699c79552e7b284dfa4205454721c5d05a80
MacBook13 <macbook13@MacBook13ui-MacBook-Pro.local>
2019-01-21 16:25:04 +0900

    마침표 수정

--- wordcount/views.py
+++ wordcount/views.py
@@ -31,26 +31,21 @@ def result(request):
     # 문장으로 구분하기
     # word안에 마침표가 찍혀있다면 마침표로 br해주겠다.
     
-    # for word in words:
-    #     word가 마침표 있어??
-    #     그러면 word에서 마침표 제외하고 그 단어 가중치 +1
-    
     # 출력할 때 가중치가 0과 1로 존재하니까
     # 1일 경우 <br>태그 추가해주자!
-    word_weight={} #가중치가 포함된 딕셔너리
     
-    
+    word_w=[] # 딕셔너리를 아이템으로 갖는 리스트
+   
+    ##### word_w 사용 
     for word in words:
-        if (word.find('.')!=-1): # 마침표가 있는 경우
-            word = word.replace(".", "")
-            word_weight[word] =0
+        if ((word.find('.')!=-1) or (word.find('!')!=-1) or (word.find('?')!=-1) ): # 마침표가 있는 경우
+            word_w.append({word : 0})
             
         else:  # 없는 경우
-            word_weight[word] = 1
-            
-     
+            word_w.append({word : 1})
+    
 
-    # 내가 추가한 작업
+   
     # 단어 숫자순 정렬
 
     # word_dic은 word : num 으로 구성되어있음
@@ -58,20 +53,16 @@ def result(request):
     sorted_dic = sorted(word_dic.items(), key=operator.itemgetter(1))
     # sorted(iterable, key, reverse)
     # param iterable
-
     # Return a new list containing all items from the iterable in ascending order.
-
     # A custom key function can be supplied to customize the sort order, and the
     # reverse flag can be set to request the result in descending order.
 
-
-    i=1
     return render(request, 'result.html', {
                                             'full':text, 
                                             'total':len(words), 
                                             'dic' : word_dic.items(),
                                             'sorted_dic' : sorted_dic ,
-                                            'word_weight' : word_weight.items(),
                                             
+                                            'word_w' : word_w,
                                             
                                             })
